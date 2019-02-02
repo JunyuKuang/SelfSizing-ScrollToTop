5d9d799671d1a83a3ae08a4e3a415789756cd50b
Jonny Kuang <lightscreen.app@gmail.com>
2019-02-02 22:48:50 +0800

    Initial Commit

---
+++ SelfSizing ScrollToTop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SelfSizing ScrollToTop
+//
+//  Created by Jonny on 2/2/19.
+//  Copyright © 2019 Junyu Kuang <lightscreen.app@gmail.com>. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
