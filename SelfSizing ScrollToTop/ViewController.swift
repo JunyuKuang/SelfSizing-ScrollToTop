7f0b4949f498a7f7706f1ef16fb79aff4fdb1acf
Jonny Kuang <lightscreen.app@gmail.com>
2019-02-02 23:01:28 +0800

    1.0

--- SelfSizing ScrollToTop/ViewController.swift
+++ SelfSizing ScrollToTop/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController : UITableViewController {
 
+    @IBAction func scrollToTop(_ sender: Any) {
+        tableView.reloadData()
+        
+        print("search results updated, now scroll to top, so user can check the latest content from top to bottom")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.tableView.setContentOffset(.zero, animated: false) // no matter true or false
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.tableView.setContentOffset(.zero, animated: false)
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.tableView.setContentOffset(.zero, animated: false)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.allowsSelection = false
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = Array(repeating: "Search Result\n", count: (1...5).randomElement()!).reduce("", +)
+        return cell
+    }
 }
 
